161bf13a714a83a4636de6dad8328e73f829fc1c
ericksonxd <117548977+ericksonxd@users.noreply.github.com>
2024-08-28 01:16:44 -0400

    Primer commit

---
+++ code.js
@@ -0,0 +1,144 @@
+let interval=null
+let comida = document.getElementById("food"), sleep = document.getElementById("sleep") , joy=document.getElementById("joy");
+let i = 100,f=100,x=100;
+let para =document.getElementById("stop");
+let sigue = document.getElementById("start");
+let darcomida = document.getElementById("plusfood");
+let dormir = document.getElementById("sueno")
+let jugar = document.getElementById("play")
+const display = document.getElementById("display")
+const toggle = document.querySelector(".toggle")
+const body = document.getElementById("bd")
+const botond = document.getElementById("botonday")
+
+
+function contador(){
+
+    if(i>100){
+        i=100
+    }
+    if(x>100){
+        x=100
+    }
+    if(f>100){
+        f=100
+    }
+comida.innerHTML = `${i=i-3}`;
+sleep.innerHTML = `${x--}`
+joy.innerHTML=`${f=f-2}`
+
+if(f <= 0 || x <= 0 || i <= 0){
+    i = "R";
+    x = "P";
+    f = "I";
+    comida.innerHTML = i;
+    sleep.innerHTML = x;
+    joy.innerHTML = f;
+    
+    clearInterval(interval); // Detiene el intervalo
+    display.setAttribute("src", "./expresiones/muerto.png");
+    return; // Termina la ejecución de la función para evitar que continúe
+}
+
+
+display.setAttribute("src", "./expresiones/normal2.png");
+setTimeout(() => {
+    display.setAttribute("src", "./expresiones/normal.png");
+}, 600);
+
+if(i<=80){
+    display.setAttribute("src","./expresiones/hambriao.png")
+}
+
+if(i<=40){
+    display.setAttribute("src","./expresiones/hambre 2.png")
+}
+
+if(x<=80){
+    display.setAttribute("src","./expresiones/sueno.png")
+}
+
+if(x<=40){
+    display.setAttribute("src","./expresiones/muchosueño.png")
+}
+
+
+
+if(f<=80){
+    display.setAttribute("src","./expresiones/triste.png")
+}
+
+if(f<=40){
+    display.setAttribute("src","./expresiones/muytriste.png")
+}
+
+let randomNumber = Math.floor(Math.random() * 10) + 1; // Número aleatorio entre 1 y 7
+if (randomNumber === 7) {
+    display.setAttribute("src", "./expresiones/random.png");
+    setTimeout(() => {
+        // Regresar a la imagen normal después de un breve tiempo
+        display.setAttribute("src", "./expresiones/normal.png");
+    }, 1000);
+}
+
+let randomNumber2 = Math.floor(Math.random() * 10) + 1; // Número aleatorio entre 1 y 7
+if (randomNumber2 === 2) {
+    display.setAttribute("src", "./expresiones/random2.png");
+    setTimeout(() => {
+        // Regresar a la imagen normal después de un breve tiempo
+        display.setAttribute("src", "./expresiones/normal.png");
+    }, 1000);
+}
+
+
+}
+
+play.addEventListener("click",()=>{
+    f=f+10
+})
+sueno.addEventListener("click",()=>{
+    x=x+10
+})
+darcomida.addEventListener("click",()=>{
+i=i+10
+})
+
+
+para.addEventListener("click",()=>{
+clearInterval(interval)
+interval=null
+})
+
+sigue.addEventListener("click",()=>{
+
+    if(interval===null){
+        interval= setInterval(contador,1000);
+    }
+
+   
+
+
+})
+
+
+toggle.addEventListener("click",()=>{
+
+body.classList.toggle("darkmode")
+toggle.classList.toggle("shine")
+const icon = document.getElementById("botonday");
+if (icon.classList.contains("fa-moon")) {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+} else {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+}
+
+})
+
+
+
+
+
+
+
