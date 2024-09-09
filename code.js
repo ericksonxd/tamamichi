4634d4505a2e3e4026a480c9d6f1ecc38588327d
ericksonxd <117548977+ericksonxd@users.noreply.github.com>
2024-09-08 21:49:15 -0400

    talking

--- code.js
+++ code.js
@@ -11,8 +11,22 @@ const toggle = document.querySelector(".toggle")
 const body = document.getElementById("bd")
 const botond = document.getElementById("botonday")
 
+let normal2 = new SpeechSynthesisUtterance("Meow Meow, Bitch")
+let normal = new SpeechSynthesisUtterance("Miau")
+let hambre = new SpeechSynthesisUtterance("Feed me");
+let hambre2 = new SpeechSynthesisUtterance("Feed me bitch I'm dying!");
+let triste = new SpeechSynthesisUtterance("Play with me");
+let triste2 = new SpeechSynthesisUtterance("Touch me unproperly");
+let sleepy = new SpeechSynthesisUtterance("I'm about to watch one piece");
+let eepy = new SpeechSynthesisUtterance("Zzzz");
+let ded = new SpeechSynthesisUtterance("fuck you I'm dead");
+
+
 
 function contador(){
+
+
+
 
     if(i>100){
         i=100
@@ -29,47 +43,80 @@ joy.innerHTML=`${f=f-2}`
 
 if(f <= 0 || x <= 0 || i <= 0){
     i = "R";
-    x = "P";
     f = "I";
+    x = "P";
     comida.innerHTML = i;
     sleep.innerHTML = x;
     joy.innerHTML = f;
     
     clearInterval(interval); // Detiene el intervalo
     display.setAttribute("src", "./expresiones/muerto.png");
+    speechSynthesis.speak(ded)
     return; // Termina la ejecución de la función para evitar que continúe
 }
 
 
+
+
 display.setAttribute("src", "./expresiones/normal2.png");
 setTimeout(() => {
+
     display.setAttribute("src", "./expresiones/normal.png");
+
+
+      
+
+ 
 }, 600);
 
 if(i<=80){
-    display.setAttribute("src","./expresiones/hambriao.png")
+    display.setAttribute("src","./expresiones/hambriao.png");
+    speechSynthesis.speak(hambre);
+    if(i>=80){
+speechSynthesis.cancel()
+    }
 }
 
 if(i<=40){
-    display.setAttribute("src","./expresiones/hambre 2.png")
+    display.setAttribute("src","./expresiones/hambre 2.png");
+    speechSynthesis.speak(hambre2)
+    if(i>=40){
+        speechSynthesis.cancel()
+            }
 }
 
 if(x<=80){
     display.setAttribute("src","./expresiones/sueno.png")
+    speechSynthesis.speak(sleepy)
+    if(x>=80){
+        speechSynthesis.cancel()
+            }
 }
 
 if(x<=40){
     display.setAttribute("src","./expresiones/mucho sueño.png")
+    speechSynthesis.speak(eepy)
+    if(x>=40){
+        speechSynthesis.cancel()
+            }
 }
 
 
 
 if(f<=80){
     display.setAttribute("src","./expresiones/triste.png")
+    speechSynthesis.speak(triste)
+    if(f>=80){
+        speechSynthesis.cancel()
+            }
 }
 
 if(f<=40){
     display.setAttribute("src","./expresiones/muytriste.png")
+    speechSynthesis.speak(triste2)
+    if(f>=40){
+        speechSynthesis.cancel()
+            }
 }
 
 let randomNumber = Math.floor(Math.random() * 10) + 1; // Número aleatorio entre 1 y 7
@@ -107,6 +154,8 @@ i=i+10
 para.addEventListener("click",()=>{
 clearInterval(interval)
 interval=null
+
+
 })
 
 sigue.addEventListener("click",()=>{
